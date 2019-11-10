0fafa7d65ad8c520089b1ec3c1773516c6d8bbae
Super <38338700+superpowers04@users.noreply.github.com>
2019-11-10 17:51:35 -0500

    Fixed some spelling mistakes

--- FixHosts.sh
+++ FixHosts.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Hello, I noticed you opened this file, You might want to open it through terminal instead
 # To make it executable on Linux type "chmod +x (directory to file)/FixHosts.sh"
 # Then to run type "sudo (directory to file)/FixHosts.sh"
@@ -6,6 +8,10 @@
 
 
 # Made by superpowers04#3887
+# With some language adjustments by JustaGhost#2197
+
+
+
 
 
 
@@ -13,15 +19,49 @@
 if [[ $( touch /etc/hosts; echo $? ) == 1 ]] 
 then
 	clear
-	echo "File needs to be run as root,"
-	echo "You can do this by putting 'sudo ' before the command"
-	echo "If you did this then you have insufficent permissions.."
-	echo "press enter to exit"
-	read
-	exit
+
+	if [[ $( zenity --error --text="File needs to be run in terminal under Root!"; echo $? ) != 0 ]]
+	then 
+		clear
+		echo "File needs to be run as root,"
+		echo "You can do this by putting 'sudo ' before the command"
+		echo "If you did this then you have insufficent permissions.."
+		echo "press enter to exit"
+		read
+		exit
+	else
+		exit
+	fi
 
 
 fi
+
+
+
+#Cut backup restore feature, cut due to read command not working correctly
+
+# if [[ -e "/etc/hostsbak" ]] 
+# then
+# 	clear
+# 	echo "A backup file from a previous fix(Or restore), Would you like to restore that backup?"
+# 	echo "Press enter to continue, Otherwise enter 'n' or 'no' to skip or "
+# 	echo "if you don't understand what this means"
+# 	read key
+
+# 	clear
+# 	echo $key
+# 	if [[ "$key" -eq "n" || "$key" -eq "no" ]]
+# 	then
+# 		cp /etc/hosts /tmp/hostsbak
+# 		cp /etc/hostsbak /etc/hosts
+# 		mv /tmp/hostsbak /etc/hostsbak
+# 		echo "All finished, Backup restored..."
+# 		read
+# 		exit
+# 	fi
+
+# fi
+
 
 #Checks for the actual issue, If not found, display message below
 
@@ -38,22 +78,22 @@ fi
 
 
 clear
-
-echo "This script will hopefully fix logging in, It will backup your hosts file"
-echo "Press enter to continue, to exit just close out of this window"
 echo ""
-echo ""
-echo ""
-echo "If interested, you can see the lines below"
+echo "Your Hosts file contains the following, which redirect Mojang Authentication to the IP shown"
 echo "---------------------------"
 cat /etc/hosts | grep "mojang"
 echo "---------------------------"
-
-
-
-
-
+echo ""
+echo "This script will remove the Mojang redirects from your hosts file"
+echo ""
+echo ""
+echo -n "Press enter to continue, to exit just close out of this window or hold Ctrl and C at the same time"
 read
+
+
+
+
+
 
 clear
 # Replaces strings
@@ -62,9 +102,7 @@ sed -i "s/authserver.mojang.com/notaealrstide.swASA/gI" /etc/hosts
 
 # All Done
 
-echo "Done, try signing in again, If it works change your password at https://minecraft.net"
-echo "If this file has damaged anything just reopen terminal and type "
-echo "'sudo cp /etc/hostsbak /etc/hosts'."
-echo "Press Enter to exit..."
+echo "Done, try signing in again, If it works change your password."
+echo -n "Press Enter to exit..."
 read
 exit
